9bfe0ca2c72030669120656d7ad0d91976b11a6f
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-19 22:47:06 -0600

    Finalizando práctica: ya guarda la imagen en la nube.

--- FirebaseStorage_Practica/Controllers/HomeController.cs
+++ FirebaseStorage_Practica/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Firebase.Auth;
+using Firebase.Storage;
 using FirebaseStorage_Practica.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -27,6 +29,36 @@ namespace FirebaseStorage_Practica.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SubirArchivo(IFormFile archivo)
+        {
+            //Leemos el archivo subido
+            Stream archivoASubir = archivo.OpenReadStream();
+
+            //Configuramos la conexion hacia FireBase
+            string email = "carlos.murga1@catolica.edu.sv";
+            string clave = "h1n12002";
+            string ruta = "dulcesabor-imagenes.appspot.com";
+            string api_key = "AIzaSyCUmhGjhkkuvkE5S5bnPXjTHIYn9qW5pl4";
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(api_key));
+            var autenticarFireBase = await auth.SignInWithEmailAndPasswordAsync(email, clave);
+
+            var cancellation = new CancellationTokenSource();
+            var tokenUser = autenticarFireBase.FirebaseToken;
+
+            var tareaCargarArchivo = new FirebaseStorage(ruta,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(tokenUser),
+                    ThrowOnCancel = true
+                }).Child("Archivos").Child(archivo.FileName).PutAsync(archivoASubir);
+
+            var urlArchivoCargado = await tareaCargarArchivo;
+
+            return RedirectToAction("VerImagen");
         }
     }
 }
